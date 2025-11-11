575c685cc8dc0785205ba07e7791a0f242119bd6
CJTULSTER <Talbot-C3@ulster.ac.uk>
2025-11-11 11:14:54 +0000

    added student and user class

---
+++ UlsterQuiz/Student.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UlsterQuiz
+{
+    internal class Student
+    {
+        private string status;
+
+        public string Status {get; set;}
+
+        public void TakeQuiz()
+        {
+
+        }
+
+        public void Feedback()
+        {
+
+        }
+    }
+}
---
+++ UlsterQuiz/User.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UlsterQuiz
+{
+    internal class User
+    {
+        private int userID;
+        private string username;
+        private string password;
+        private string email;
+        private string role;
+
+        public int UserID {get; set;}
+        public string Username {get; set;}
+        public string Password {get; set;}
+        public string Email {get; set;}
+        public string Role {get; set;}
+
+        public void UpdateProfile()
+        {
+
+        }
+
+        public void Login()
+        {
+
+        }
+
+        public void Logout()
+        {
+
+        }
+
+    }
+}
